319a07010a14abd3d609956e01979d17d9c36980
Damego <danyabatueff@gmail.com>
2023-07-15 22:04:07 +0500

    refactor: remove test code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 from os import environ
 
 from telegram import KeyboardButton, ReplyKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import ContextTypes, ConversationHandler, filters
+from telegram.ext import ContextTypes, filters
 from dotenv import load_dotenv
 
 from handlers.decorators import callback_query, command, message, startup
@@ -14,10 +14,8 @@ import logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-logger = logging.getLogger(__name__)
 load_dotenv()
 
 client = Client()
@@ -30,134 +28,9 @@ SUBSCRIPTIONS_BUTTON = "❤️ Подписки"
 ignore_text = [SEARCH_BUTTON, SUBSCRIPTIONS_BUTTON]
 
 
-def generate_state(length: int):
-    return map(str, range(length))
+conversation = Conversation()
 
-
-GENDER, PHOTO, LOCATION, BIO = generate_state(4)
-
-conv = Conversation()
-
-
-@conv.state(GENDER)
-@command()
-async def gen(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    return PHOTO
-
-
-@conv.entry_point()
-@command()
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [["Boy", "Girl", "Other"]]
-
-    await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
-    )
-
-    return GENDER
-
-
-@conv.state(GENDER)
-@message(filters.Regex("^(Boy|Girl|Other)$"))
-async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected gender and asks for a photo."""
-    user = update.message.from_user
-
-    await update.message.reply_text(
-        "I see! Please send me a photo of yourself, "
-        "so I know what you look like, or send /skip if you don't want to.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    return PHOTO
-
-
-@conv.state(PHOTO)
-@message(filters.PHOTO)
-async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = await update.message.photo[-1].get_file()
-    await photo_file.download_to_drive("user_photo.jpg")
-
-    await update.message.reply_text(
-        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
-    )
-
-    return LOCATION
-
-
-@conv.state(PHOTO)
-@command("skip", register=False)
-async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-
-    await update.message.reply_text(
-        "I bet you look great! Now, send me your location please, or send /skip."
-    )
-
-    return LOCATION
-
-
-@conv.state(LOCATION)
-@message(filters.LOCATION)
-async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the location and asks for some info about the user."""
-    user = update.message.from_user
-    user_location = update.message.location
-
-    await update.message.reply_text(
-        "Maybe I can visit you sometime! At last, tell me something about yourself."
-    )
-
-    return BIO
-
-
-@conv.state(LOCATION)
-@command("skip", register=False)
-async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-
-    await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
-    )
-
-    return BIO
-
-
-@conv.state(BIO)
-@message(filters.TEXT & ~filters.COMMAND)
-async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
-
-    return ConversationHandler.END
-
-
-@conv.fallback()
-@command()
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-    )
-
-    return ConversationHandler.END
-
-
-SEARCH, SELECT = generate_state(2)
+START, SEARCH, SELECT, SUBSCRIBE = conversation.States.generate_state(4)
 
 
 @startup
@@ -165,58 +38,80 @@ async def on_startup():
     print(f"{client.bot.username} started")
 
 
+@conversation.entry_point()
 @command()
-async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[KeyboardButton(SEARCH_BUTTON), KeyboardButton(SUBSCRIPTIONS_BUTTON)]]
 
     await update.message.reply_text(
         "Хай", reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     )
 
+    return START
 
+
+@conversation.state(START)
 @message(filters.Text([SEARCH_BUTTON]))
 async def find_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    active_search.append(update.effective_user.id)
-
     await update.message.reply_text("Введите название тайтла")
 
     return SEARCH
 
 
-# @message(filters.TEXT & ~filters.COMMAND)
-# async def process_search(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     print(1)
-#     if update.effective_user.id not in active_search:
-#         return
-#     active_search.remove(update.effective_user.id)
-#
-#     text = update.message.text
-#     if text in ignore_text:
-#         return
-#
-#     titles = await read_manga.search(text)
-#
-#     keyboard = [
-#         [
-#             InlineKeyboardButton(title["name"], callback_data=f"search:{title['url']}")
-#         ]
-#         for title in titles
-#     ]
-#
-#     await update.message.reply_text(f"Найдено {len(titles)} тайтлов", reply_markup=InlineKeyboardMarkup(keyboard))
-#
+@conversation.state(SEARCH)
+@message(filters.TEXT & ~filters.COMMAND)
+async def process_search(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    if text in ignore_text:
+        print("nope")
+        return
+
+    titles = await read_manga.search(text)
+
+    keyboard = []
+    context.user_data["manga"] = {}
+
+    for i in range(len(titles)):
+        keyboard.append(
+            [InlineKeyboardButton(titles[i]["name"], callback_data=f"search:{i}")]
+        )
+        context.user_data["manga"][i] = titles[i]['url']
+
+    await update.message.reply_text(f"Найдено {len(titles)} тайтлов", reply_markup=InlineKeyboardMarkup(keyboard))
+
+    return SELECT
 
 
+@conversation.state(SELECT)
 @callback_query(re.compile("search:"))
 async def on_title_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    urn = update.callback_query.data.removeprefix("search:")
+    index = update.callback_query.data.removeprefix("search:")
+    urn = context.user_data["manga"][int(index)]
+
     data = await read_manga.get_latest_chapter(urn)
 
     keyboard = [
         [
-            InlineKeyboardButton("Подписаться", callback_data=f"sub:{urn}")
+            InlineKeyboardButton("Подписаться", callback_data=f"sub:{index}")
         ]
     ]
-    await update.callback_query.answer(f"{data['name']}\nПоследняя глава: {data['volume']}-{data['chapter']}")
+    await update.callback_query.answer()
+    await update.effective_chat.send_message(
+        f"{data['name']}\nПоследняя глава: {data['volume']}-{data['chapter']}",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+    return SUBSCRIBE
+
+
+@conversation.state(SUBSCRIBE)
+@callback_query(re.compile("sub:"))
+async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    index = update.callback_query.data.removeprefix("sub:")
+    print("подписано на", index)
+    await update.callback_query.answer()
+
+    return conversation.States.END
+
 
 client.run(environ["TELEGRAM_BOT_TOKEN"])
